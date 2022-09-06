c18006ff6568628c57483515a7a33620d806e58b
Vinicius Castro <vini-freitascastro@hotmail.com>
2022-09-06 02:57:10 -0300

    Codando no linux

--- ide.cpp
+++ ide.cpp
@@ -1,15 +1,12 @@
+// g++ -g -Wall ide.cpp -o ide
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
 
-#ifdef _WIN32
-#define CLEAR "cls"
-#else
-#define CLEAR "clear"
-#endif
-
 void escreveoarquivo(){
+    system("clear");
     FILE * x = fopen("codigo.txt", "r");
     char aux = fgetc(x);
     while(aux!=EOF){
@@ -19,26 +16,28 @@ void escreveoarquivo(){
 }
 
 int main() {
-    char input, aux = '|', ant = '|';
+    char input = '.', aux = '|', anterior = '|';
     FILE* backup = NULL, * help = NULL;
     int i = 1, j, k;
     backup = fopen("codigo.txt", "w");
-    fprintf(backup, "IDE EM C - PARA PARAR DE DIGITAR O CODIGO UTILIZE O CARACTERE '|'.\n%03d - ", i);
+    fprintf(backup, "\n\n\tIDE EM C - PARA PARAR DE DIGITAR O CODIGO UTILIZE O CARACTERE '|'.\n%03d - ", i);
     fclose(backup);
+    escreveoarquivo();
     do {
-        system(CLEAR);
-        escreveoarquivo();
         backup = fopen("codigo.txt", "a+");
-        input = _getch();
+        printf("%c", anterior);
+        escreveoarquivo();
+        input = getch();
+        
         switch (input) {
-            case 13:
+            case '\n':
                 fprintf(backup, "\n");
                 i++;
                 fprintf(backup, "%03d - ", i);
                 break;
-            case 8:
+            case 127:
                 help = fopen("help.txt", "w");
-                fseek(backup,-1-i,SEEK_END);
+                fseek(backup,-1,SEEK_END);
                 k = ftell(backup);
                 fseek(backup,0,SEEK_SET);
                 for(j=0; j<k; j++) {
@@ -65,9 +64,9 @@ int main() {
                     fprintf(backup, "%c", input);
                 break;
         }
+        anterior = input;
+        fclose(backup);     
         
-        fclose(backup);
-        ant = input;
     } while (input != '|');
 
     return 0;
